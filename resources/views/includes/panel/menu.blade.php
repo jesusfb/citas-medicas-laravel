575084ce13262ada3009c3d37365274daf222cb1
Jorge Luis <jorgestudio2017@gmail.com>
2020-02-15 21:01:40 -0400

    Subiendo proyecto y plantilla

---
+++ resources/views/includes/panel/menu.blade.php
@@ -0,0 +1,55 @@
+<!-- Navigation -->
+<h6 class="navbar-heading text-muted">Gestionar Datos</h6>
+<ul class="navbar-nav">
+    <li class="nav-item  active ">
+      <a class="nav-link  active " href="./index.html">
+        <i class="ni ni-tv-2 text-primary"></i> Dashboard
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link " href="./examples/icons.html">
+        <i class="ni ni-planet text-blue"></i> Especialidades
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link " href="./examples/maps.html">
+        <i class="ni ni-single-02 text-orange"></i>  Medicos
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link " href="./examples/profile.html">
+        <i class="ni ni-satisfied text-info"></i>  Pacientes
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link " href="./examples/tables.html">
+        <i class="ni ni-calendar-grid-58 text-red"></i>  Horarios
+      </a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('formLogout').  submit();">
+          <i class="ni ni-key-25"></i> Cerrar Sesión
+        </a>
+    <form action="{{route('logout')}}" method="POST" style="display:none;" id="formLogout">
+         @csrf
+    </form>
+      </li>
+  </ul>
+  <!-- Divider -->
+  <hr class="my-3">
+  <!-- Heading -->
+  <h6 class="navbar-heading text-muted">Reportes</h6>
+  <!-- Navigation -->
+  <ul class="navbar-nav mb-md-3">
+    <li class="nav-item">
+      <a class="nav-link" href="#">
+        <i class="ni ni-collection text-yellow"></i> Frecuencia de Citas
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">
+        <i class="ni ni-palette text-pink"></i> Médicos mas Activos
+      </a>
+    </li>
+  </ul>
+  <ul class="navbar-nav">
